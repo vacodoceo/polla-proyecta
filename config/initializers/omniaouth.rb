212e78398b346959a64699f7f9721976b088f088
Verner Codoceo <vacodoceo@uc.cl>
2019-05-08 10:23:40 -0400

    Add email info to google login

--- config/initializers/omniaouth.rb
+++ config/initializers/omniaouth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_ID'], ENV['GOOGLE_SECRET'], {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}, scope: "userinfo.email", skip_jwt: true}
+  provider :google_oauth2, ENV['GOOGLE_ID'], ENV['GOOGLE_SECRET'], {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}, scope: "userinfo.email, userinfo.profile", skip_jwt: true, provider_ignores_state: true}
 end
