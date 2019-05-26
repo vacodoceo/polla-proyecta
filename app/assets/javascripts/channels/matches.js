16d56d2fd4731a5c030424ffc9579b7bcedd7d0d
Verner Codoceo <vacodoceo@uc.cl>
2019-05-25 23:49:21 -0400

    Add quarter finals second country handler

--- app/assets/javascripts/channels/matches.js
+++ app/assets/javascripts/channels/matches.js
@@ -5,6 +5,9 @@ $(document).on("turbolinks:load", function(){
 
     handleGroups();
 
+    handleCountrySelect(1);
+    handleCountrySelect(4);
+
     $("input[type='number']").inputSpinner();
 
     
@@ -21,15 +24,37 @@ function handleGroups(){
         }
 
         addCountry(1, groups['A'][0], [groups['B'][2], groups['C'][2]]);
-        addCountry(2, groups['B'][0], [groups['C'][0]]);
-        addCountry(3, groups['A'][1], [groups['B'][1]]);
+        addCountry(2, groups['B'][0], groups['C'][1]);
+        addCountry(3, groups['A'][1], groups['B'][1]);
         addCountry(4, groups['C'][0], [groups['A'][2], groups['B'][2]]);
     } );
 }
 
-function addCountry(group, c1, c2_array){
+function addCountry(group, c1, c2){
     let c1_name = $('#'+c1+' .country-name').text();
     $('#'+group.toString()+' li:nth-child(2) span:first-child').removeClass();
     $('#'+group.toString()+' li:nth-child(2) span:first-child').addClass('flag-icon flag-icon-'+c1);
     $('#'+group.toString()+' li:nth-child(2) span:nth-child(2)').html(c1_name);
+    if (group == 1 || group == 4){
+        $('#'+group.toString()+' li:nth-child(3) .select-country')
+            .find('option')
+            .remove()
+            .end()
+            .append('<option value="'+c2[0]+'">'+$('#'+c2[0]+' .country-name').text()+'</option>')
+            .append('<option value="'+c2[1]+'">'+$('#'+c2[1]+' .country-name').text()+'</option>')
+;
+    }
+    else {
+        let c2_name = $('#'+c2+' .country-name').text();
+        $('#'+group.toString()+' li:nth-child(3) span:first-child').removeClass();
+        $('#'+group.toString()+' li:nth-child(3) span:first-child').addClass('flag-icon flag-icon-'+c2);
+        $('#'+group.toString()+' li:nth-child(3) span:nth-child(2)').html(c2_name);
+    }
+}
+
+function handleCountrySelect(group){
+    $('#'+group.toString()+ ' .select-country').change(function() {
+        $('#'+group.toString()+' li:nth-child(3) span:first-child').removeClass();
+        $('#'+group.toString()+' li:nth-child(3) span:first-child').addClass('flag-icon flag-icon-'+$(this).val());
+    });
 }
