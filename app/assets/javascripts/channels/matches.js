85f00b3de12f249eaaebb107061b9a15b5af782e
Verner Codoceo <vacodoceo@uc.cl>
2019-05-26 02:48:56 -0400

    Add tie winner option

--- app/assets/javascripts/channels/matches.js
+++ app/assets/javascripts/channels/matches.js
@@ -25,7 +25,7 @@ function handleGroups(){
             }
         }
 
-        console.log(groups);
+        // console.log(groups);
 
         addCountry(1, groups['A'][0], [groups['C'][2], groups['B'][2]]);
         addCountry(2, groups['B'][0], groups['C'][1]);
@@ -35,7 +35,7 @@ function handleGroups(){
 }
 
 function addCountry(match, c1, c2){
-    console.log(match + " " + c1 + " " + c2);
+    // console.log(match + " " + c1 + " " + c2);
     let c1_name = $('#'+c1+' .country-name').text();
     $('#'+match.toString()+' .groups li:nth-child(1) span:first-child').removeClass();
     $('#'+match.toString()+' .groups li:nth-child(1) span:first-child').addClass('flag-icon flag-icon-'+c1);
@@ -70,17 +70,45 @@ function handleWinner(){
     $('input#number').on("change", function () {
         let country1 = $(this).parent().parent().children('li:nth-child(1)'); 
         let country2 = $(this).parent().parent().children('li:nth-child(2)'); 
+        let standings = country1.parent().parent().children('.standings');
         if (country1.children('input#number').val() > country2.children('input#number').val()){
             country1.addClass('list-group-item-success');
             country2.removeClass('list-group-item-success');
+            standings.children().children().addClass('disabled');
+            standings.children('li:nth-child(2)').children().removeClass('btn-success');
+            standings.children('li:nth-child(2)').children().addClass('btn-outline-success');
+            standings.children('li:nth-child(1)').children().removeClass('btn-outline-success');
+            standings.children('li:nth-child(1)').children().addClass('btn-success');
         }
         else if (country1.children('input#number').val() < country2.children('input#number').val()){
             country2.addClass('list-group-item-success');
             country1.removeClass('list-group-item-success');
+            standings.children().children().addClass('disabled');
+            standings.children('li:nth-child(1)').children().removeClass('btn-success');
+            standings.children('li:nth-child(1)').children().addClass('btn-outline-success');
+            standings.children('li:nth-child(2)').children().removeClass('btn-outline-success');
+            standings.children('li:nth-child(2)').children().addClass('btn-success');
         }
         else {
             country1.removeClass('list-group-item-success');
             country2.removeClass('list-group-item-success');
+            standings.children().children().removeClass('disabled');
+            standings.children().children().removeClass('btn-success');
+            standings.children().children().addClass('btn-outline-success');
+        }
+    })
+
+    $('.button-winner').click(function () {
+        if (!$(this).hasClass('disabled')){
+            $(this).removeClass('btn-outline-success');
+            $(this).addClass('btn-success');
+            let my_pos = (parseInt($(this).attr('id'))).toString();
+            let other_pos = (3 - parseInt($(this).attr('id'))).toString();
+            $(this).parent().parent().children('li:nth-child('+other_pos+')').children().removeClass('btn-success');
+            $(this).parent().parent().children('li:nth-child('+other_pos+')').children().addClass('btn-outline-success');
+
+            $(this).parent().parent().parent().children('ul:nth-child(2)').children('li:nth-child('+my_pos+')').addClass('list-group-item-success');
+            $(this).parent().parent().parent().children('ul:nth-child(2)').children('li:nth-child('+other_pos+')').removeClass('list-group-item-success');
         }
     })
 }
