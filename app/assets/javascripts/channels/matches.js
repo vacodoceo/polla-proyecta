95717fcce1f0340eda002eb9c0cf319bf6ff26ab
Verner Codoceo <vacodoceo@uc.cl>
2019-05-26 01:32:32 -0400

    Fix bug with flags and automatic color change on winner

--- app/assets/javascripts/channels/matches.js
+++ app/assets/javascripts/channels/matches.js
@@ -16,7 +16,7 @@ $(document).on("turbolinks:load", function(){
 });
 
 function handleGroups(){
-    $(".sortable").on( "sortupdate", function( event, ui ) {
+    $(".sortable").on( "sortupdate", function() {
         groups = { 'A': [], 'B':[], 'C':[] };
         for (i=0; i<Object.keys(groups).length; i++){
             for (j=0; j<4; j++){
@@ -25,20 +25,23 @@ function handleGroups(){
             }
         }
 
-        addCountry(1, groups['A'][0], [groups['C'][2]], groups['B'][2]);
+        console.log(groups);
+
+        addCountry(1, groups['A'][0], [groups['C'][2], groups['B'][2]]);
         addCountry(2, groups['B'][0], groups['C'][1]);
         addCountry(3, groups['A'][1], groups['B'][1]);
         addCountry(4, groups['C'][0], [groups['A'][2], groups['B'][2]]);
     } );
 }
 
-function addCountry(group, c1, c2){
+function addCountry(match, c1, c2){
+    console.log(match + " " + c1 + " " + c2);
     let c1_name = $('#'+c1+' .country-name').text();
-    $('#'+group.toString()+' li:nth-child(2) span:first-child').removeClass();
-    $('#'+group.toString()+' li:nth-child(2) span:first-child').addClass('flag-icon flag-icon-'+c1);
-    $('#'+group.toString()+' li:nth-child(2) span:nth-child(2)').html(c1_name);
-    if (group == 1 || group == 4){
-        $('#'+group.toString()+' li:nth-child(3) .select-country')
+    $('#'+match.toString()+' .groups li:nth-child(1) span:first-child').removeClass();
+    $('#'+match.toString()+' .groups li:nth-child(1) span:first-child').addClass('flag-icon flag-icon-'+c1);
+    $('#'+match.toString()+' .groups li:nth-child(1) span:nth-child(2)').html(c1_name);
+    if (match == 1 || match == 4){
+        $('#'+match.toString()+' .groups li:nth-child(3) .select-country')
             .find('option')
             .remove()
             .end()
@@ -46,27 +49,27 @@ function addCountry(group, c1, c2){
     }
     else {
         let c2_name = $('#'+c2+' .country-name').text();
-        $('#'+group.toString()+' li:nth-child(3) span:first-child').removeClass();
-        $('#'+group.toString()+' li:nth-child(3) span:first-child').addClass('flag-icon flag-icon-'+c2);
-        $('#'+group.toString()+' li:nth-child(3) span:nth-child(2)').html(c2_name);
+        $('#'+match.toString()+' .groups li:nth-child(2) span:first-child').removeClass();
+        $('#'+match.toString()+' .groups li:nth-child(2) span:first-child').addClass('flag-icon flag-icon-'+c2);
+        $('#'+match.toString()+' .groups li:nth-child(2) span:nth-child(2)').html(c2_name);
     }
 }
 
-function handleCountrySelect(group){
-    $('#'+group.toString()+ ' .select-country').change(function() {
-        $('#'+group.toString()+' li:nth-child(3) span:first-child').removeClass();
-        $('#'+group.toString()+' li:nth-child(3) span:first-child').addClass('flag-icon flag-icon-'+$(this).val());
-        other_group = (group == 1) ? 4 : 1;
-        $('#'+other_group.toString()+' li:nth-child(3) .select-country option').css({ 'display': ''});
-        $('#'+other_group.toString()+' li:nth-child(3) .select-country .'+$(this).val()).hide();
+function handleCountrySelect(match){
+    $('#'+match.toString()+ ' .select-country').change(function() {
+        $('#'+match.toString()+' .groups li:nth-child(2) span:first-child').removeClass();
+        $('#'+match.toString()+' .groups li:nth-child(2) span:first-child').addClass('flag-icon flag-icon-'+$(this).val());
+        other_match = (match == 1) ? 4 : 1;
+        $('#'+other_match.toString()+' .groups li:nth-child(2) .select-country option').css({ 'display': ''});
+        $('#'+other_match.toString()+' .groups li:nth-child(2) .select-country .'+$(this).val()).hide();
         }
     )    
 }
 
 function handleWinner(){
     $('input#number').on("change", function () {
-        let country1 = $(this).parent().parent().children('li:nth-child(2)'); 
-        let country2 = $(this).parent().parent().children('li:nth-child(3)'); 
+        let country1 = $(this).parent().parent().children('li:nth-child(1)'); 
+        let country2 = $(this).parent().parent().children('li:nth-child(2)'); 
         if (country1.children('input#number').val() > country2.children('input#number').val()){
             country1.addClass('list-group-item-success');
             country2.removeClass('list-group-item-success');
