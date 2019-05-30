e751cefc6cf93b016c69f8ba72aaf043047059a5
Verner Codoceo <vacodoceo@uc.cl>
2019-05-30 05:10:25 -0400

    v0.0.1

--- app/controllers/pollas_controller.rb
+++ app/controllers/pollas_controller.rb
@@ -69,8 +69,8 @@ class PollasController < ApplicationController
           send_email: true,
           payer_name: current_user.name,
           payer_email: current_user.email,
-          return_url: 'localhost:3000/pollas',
-          cancel_url: 'localhost:3000',
+          return_url: pollas_path,
+          cancel_url: root_path,
           #notify_url: 'http://mi-ecomerce.com/backend/notify',
           notify_api_version: '1.3'
       })
