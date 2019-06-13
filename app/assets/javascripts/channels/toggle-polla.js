04e61c7f0088aaacba1c6303e5e42181ff76476c
Verner Codoceo <vacodoceo@uc.cl>
2019-06-13 04:27:24 -0400

    Fix bug with match 2 and 3 show

--- app/assets/javascripts/channels/toggle-polla.js
+++ app/assets/javascripts/channels/toggle-polla.js
@@ -24,11 +24,11 @@ function build_polla(data){
     buildGroup('A', first_round.slice(8, 12).reverse())
 
     for (i=1; i<9; i++){
-        if (i == 3){
-            buildMatch($(".list-group-match#"+i+" .list-group.groups:last-child"), data.matches[3]);
-        }
-        else if (i == 4){
+        if (9-i-1 == 3){
             buildMatch($(".list-group-match#"+i+" .list-group.groups:last-child"), data.matches[2]);
+        }
+        else if (9-i-1 == 2){
+            buildMatch($(".list-group-match#"+i+" .list-group.groups:last-child"), data.matches[3]);
         }
         else{
             buildMatch($(".list-group-match#"+i+" .list-group.groups:last-child"), data.matches[9-i-1]);
